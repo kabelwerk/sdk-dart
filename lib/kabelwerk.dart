f15383dda147484800832cd0b411b03c5c323645
pavelsof <mail@pavelsof.com>
2022-01-21 17:06:09 +0100

    First commit.
    dart create --template package-simple kabelwerk

---
+++ lib/kabelwerk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library kabelwerk;
+
+export 'src/kabelwerk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
