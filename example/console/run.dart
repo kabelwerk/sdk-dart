4e545009fc1592413f85bff506d98e68f396a502
pavelsof <mail@pavelsof.com>
2023-02-21 09:23:16 +0100

    Updated the example console app.

--- example/console/run.dart
+++ example/console/run.dart
@@ -5,28 +5,25 @@ import 'package:args/args.dart';
 import 'package:kabelwerk/kabelwerk.dart';
 
 void main(List<String> arguments) {
-  final parser = ArgParser()
-    ..addOption('url', mandatory: true)
-    ..addOption('token', mandatory: true);
-
-  final parsed = parser.parse(arguments);
+  final parsed = parseArguments(arguments);
 
   final kabelwerk = Kabelwerk();
-  Room? room;
+  late Room room;
 
   kabelwerk.config
     ..url = parsed['url']
     ..token = parsed['token'];
 
-  kabelwerk.on('connected', (_) => printStatus('Kabelwerk — connected'));
-  kabelwerk.on('disconnected', (_) => printStatus('Kabelwerk — disconnected'));
-  kabelwerk.on('reconnected', (_) => printStatus('Kabelwerk — reconnected'));
+  kabelwerk.on('connected',
+      (ConnectedEvent event) => printStatus('Kabelwerk — connected'));
+  kabelwerk.on('disconnected',
+      (DisconnectedEvent event) => printStatus('Kabelwerk — disconnected'));
 
-  kabelwerk.on('ready', (_) {
-    printStatus('Kabelwerk — ­ready');
+  kabelwerk.on('ready', (KabelwerkReadyEvent event) {
+    printStatus('Kabelwerk — ready');
 
     kabelwerk.openInbox()
-      ..on('ready', (event) {
+      ..on('ready', (InboxReadyEvent event) {
         printStatus('Inbox — ready');
 
         if (event.items.isEmpty) {
@@ -34,10 +31,10 @@ void main(List<String> arguments) {
             room = setupRoom(kabelwerk, roomId);
           });
         } else {
-          room = setupRoom(kabelwerk, event.items.first.room.id);
+          room = setupRoom(kabelwerk, event.items.first.roomId);
         }
       })
-      ..on('updated', (event) {
+      ..on('updated', (InboxUpdatedEvent event) {
         printStatus('Inbox — updated');
       })
       ..connect();
@@ -48,7 +45,7 @@ void main(List<String> arguments) {
   void handleLine(line) {
     if (line.startsWith('/name ')) {
     } else {
-      room?.postMessage(text: line);
+      room.postMessage(text: line);
     }
   }
 
@@ -60,24 +57,32 @@ void main(List<String> arguments) {
 
 Room setupRoom(Kabelwerk kabelwerk, int roomId) {
   return kabelwerk.openRoom(roomId)
-    ..on('ready', (event) {
+    ..on('ready', (RoomReadyEvent event) {
       printStatus('Room — ready');
 
       for (final message in event.messages) {
         printMessage(message);
       }
     })
-    ..on('message_posted', (event) {
+    ..on('message_posted', (MessagePostedEvent event) {
       printMessage(event.message);
     })
     ..connect();
 }
 
+ArgResults parseArguments(List<String> arguments) {
+  final parser = ArgParser()
+    ..addOption('url', mandatory: true)
+    ..addOption('token', mandatory: true);
+
+  return parser.parse(arguments);
+}
+
 void printStatus(String status) {
-  stdout.writeln('* ${status}');
+  stdout.writeln('* $status');
 }
 
 void printMessage(Message message) {
   stdout.writeln(
-      '[${message.insertedAt}] <${message.user?.name}> ${message.text}');
+      '[${message.insertedAt}] <${message.user.name}> ${message.text}');
 }
